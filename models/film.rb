e6267ab1422d9e0054f04630067b5df67b5f5d52
Paul Milne <hallhill@gmail.com>
2016-10-15 13:45:57 +0100

    created crud methods for films

--- models/film.rb
+++ models/film.rb
@@ -11,4 +11,34 @@ class Film
         @running_time = options['running_time'].to_i
     end
 
+    def create
+        sql = "INSERT INTO films (name, genre, running_time) VALUES (
+            name            = '#{name}',
+            genre           = '#{genre}',
+            running_time    = '#{running_time}')
+        RETURNING *;"
+        film = SqlRunner.run(sql).first
+        @id = film['id']
+    end
+
+    def self.read_all
+        sql = "SELECT * FROM films"
+        all_films = SqlRunner.run(sql).map {|film| Film.new(film)}
+        return all_films
+    end
+
+    def update
+        sql = "UPDATE films SET (
+            name = '#{@name}',
+            genre = '#{@genre}',
+            running_time = '#{@running_time}') 
+        WHERE id = #{@id};"
+        SqlRunner.run(sql)
+    end
+
+    def delete
+        sql = "DELETE FROM films WHERE id = #{@id}"
+        SqlRunner.run(sql)
+    end
+
 end
